64d366cccd34131dbfad23a583917f71c665f220
Pacifique Twagirayesu <pacifiquetwagirayesu520@gmail.com>
2025-10-25 23:29:49 +0800

    oauth server

--- src/main/java/com/pacifique/resourceserver/ResourceServerApplication.java
+++ src/main/java/com/pacifique/resourceserver/ResourceServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsiaCh15Ex1Application {
+public class ResourceServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SsiaCh15Ex1Application.class, args);
+        SpringApplication.run(ResourceServerApplication.class, args);
     }
 
 }
